b3dbd989654357f021782e431128d866e3917231
OlehPerlovych <perlovich92@gmail.com>
2023-10-24 16:06:31 +0300

    refactoring and Add require.context for rendering images.

--- src/components/Friends.js
+++ src/components/Friends.js
@@ -8,6 +8,7 @@ const importedImages = imageFiles.map(images);
 const Friends = () =>{
     return (
          <section className="float-end w-50 border border-light rounded-bottom-3 row m-1">
+             <h3 className="text-center col-12 text-uppercase">Friends</h3>
             {importedImages.map((img,index)=> <Friend image={img} key={index}/>)}
          </section>
     )
