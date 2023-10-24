3f35c6e55aeaa5506e02aea49e04b5a746db7098
OlehPerlovych <perlovich92@gmail.com>
2023-10-24 18:22:28 +0300

    improvement of Hero in import of pictures

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import hero from '../styles/images/main.jpg';
 
 const Hero = () => {
     return (
         <section className="float-start w-25 my-1 mx-3">
-            <img className="w-100" src={hero} alt="hero" />
+            <img src="../styles/images/main.jpg" className="w-100" alt={'hero'}/>
         </section>
     );
 };
