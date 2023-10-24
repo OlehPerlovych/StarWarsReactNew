60fdbf2a9fd63741a9c23706dd0ee73a19cc1b5a
OlehPerlovych <perlovich92@gmail.com>
2023-10-24 18:36:44 +0300

    import of picture in Hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import hero from '../styles/images/main.jpg';
 
 const Hero = () => {
     return (
         <section className="float-start w-25 my-1 mx-3">
-            <img className="w-100" src={hero} alt="hero" />
+            <img className="w-100" src={require('../styles/images/main.jpg')} alt="hero" />
         </section>
     );
 };
