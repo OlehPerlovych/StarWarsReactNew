3e7dc51149f8d34fc4f2860f7732b1356d1886da
OlehPerlovych <perlovich92@gmail.com>
2023-10-24 16:09:06 +0300

    Add <h3> Friends

--- src/components/Friends.js
+++ src/components/Friends.js
@@ -13,15 +13,6 @@ const Friends = () =>{
          </section>
     )
 }
-//
-//
-// const Friends = () => {
-//     return (
-//         <section className="float-end w-50 border border-light rounded-bottom-3 row m-1">
-//             <h3 className="text-center col-12 text-uppercase">Friends</h3>
-//             <Friend/>
-//         </section>
-//     );
-// };
+
 
 export default Friends;
