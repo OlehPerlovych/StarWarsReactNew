ba759fb0e188152cbad51f9df5e0d0a011d642e2
OlehPerlovych <perlovich92@gmail.com>
2023-10-24 18:42:08 +0300

    Add footer.module.css and import style to Footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,13 +1,13 @@
 import React from 'react';
+import style from './footer.module.css';
 
 const Footer = () => {
     return (
         <footer className="rounded-bottom-4">
             <div className="nav-item btn btn-danger border border-light rounded-pill mx-1 common-button my-3">
-                <p>Send me an <span className="email">email</span></p>
+                <p>Send me an <span className={style.email}>email</span></p>
             </div>
         </footer>
     );
 };
-
 export default Footer;
