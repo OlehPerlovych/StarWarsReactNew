58d8b8f3679db165dda660482b3e16aa9c8a1d62
OlehPerlovych <perlovich92@gmail.com>
2023-10-28 22:41:22 +0300

    Home Work

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,7 +1,38 @@
 import React, {Component} from 'react';
+import {period} from "../utils/constants";
 
 class AboutMe extends Component
 {
+
+    constructor(props) {
+        super(props);
+        this.state =
+            {
+                "name": null,
+                "birth_year": null,
+                "gender": null,
+                "eye_color": null,
+                "hair_color": null,
+                "skin_color": null,
+                "height": null,
+                "mass": null
+            }
+    }
+
+    componentDidMount()
+    {
+        const hero = JSON.parse(localStorage.getItem('hero'));
+
+        if(!hero || (Date.now()- hero.timeStamp) > period)
+        {
+            fetch()
+        }
+        else
+        {
+            this.setState(hero.info);
+        }
+    }
+
     render()
     {
         return (
