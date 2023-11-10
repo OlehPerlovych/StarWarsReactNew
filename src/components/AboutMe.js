03b8a5161193c18e2a6299668f5db44b9b3047ce
OlehPerlovych <perlovich92@gmail.com>
2023-11-10 08:54:03 +0200

    move to Hooks

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,57 +1,51 @@
-import React, { Component } from 'react';
-import {HERO, period, url} from "../utils/constants";
+import React, { useState, useEffect } from 'react';
+import { HERO, period, url } from '../utils/constants';
 
-class AboutMe extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            "name": null,
-            "birth_year": null,
-            "gender": null,
-            "eye_color": null,
-            "hair_color": null,
-            "skin_color": null,
-            "height": null,
-            "mass": null
-        }
-    }
+function AboutMe() {
+    const [heroInfo, setHeroInfo] = useState({
+        name: null,
+        birth_year: null,
+        gender: null,
+        eye_color: null,
+        hair_color: null,
+        skin_color: null,
+        height: null,
+        mass: null,
+    });
 
-    componentDidMount() {
+    useEffect(() => {
         const hero = JSON.parse(localStorage.getItem(HERO));
 
-        if (!hero || (Date.now() - hero.timeStamp) > period) {
-            this.fetchDataFromAPI();
+        const fetchDataFromAPI = () => {
+            fetch(`${url}peoples/1`)
+                .then((response) => response.json())
+                .then((data) => {
+                    localStorage.setItem(HERO, JSON.stringify({ info: data, timeStamp: Date.now() }));
+                    setHeroInfo(data);
+                })
+                .catch((error) => alert(error));
+        };
+
+        if (!hero || Date.now() - hero.timeStamp > period) {
+            fetchDataFromAPI();
         } else {
-            this.setState(hero.info);
+            setHeroInfo(hero.info);
         }
-    }
+    }, []);
 
-    fetchDataFromAPI()
-    {
-        fetch(`${url}peoples/1`)
-            .then((response) => response.json())
-            .then((data) =>
-            {
-                localStorage.setItem(HERO, JSON.stringify({ info: data, timeStamp: Date.now()}));
-                this.setState(data);
-            })
-            .catch(error => alert(error));
-    };
-
-    render() {
-        return (
-            <div>
-                <h1>About Me</h1>
-                <p>Name: {this.state.name}</p>
-                <p>Birth Year: {this.state.birth_year}</p>
-                <p>Gender: {this.state.gender}</p>
-                <p>Eye Color: {this.state.eye_color}</p>
-                <p>Hair Color: {this.state.hair_color}</p>
-                <p>Skin Color: {this.state.skin_color}</p>
-                <p>Height: {this.state.height}</p>
-                <p>Mass: {this.state.mass}</p>
-            </div>
-        );
-    }
+    return (
+        <div>
+            <h1>About Me</h1>
+            <p>Name: {heroInfo.name}</p>
+            <p>Birth Year: {heroInfo.birth_year}</p>
+            <p>Gender: {heroInfo.gender}</p>
+            <p>Eye Color: {heroInfo.eye_color}</p>
+            <p>Hair Color: {heroInfo.hair_color}</p>
+            <p>Skin Color: {heroInfo.skin_color}</p>
+            <p>Height: {heroInfo.height}</p>
+            <p>Mass: {heroInfo.mass}</p>
+        </div>
+    );
 }
+
 export default AboutMe;
--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,79 +1,69 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from './contact.module.css';
-import {LAST_UPDATE, periodForPlanets, PLANETS, url} from "../../utils/constants";
+import { LAST_UPDATE, periodForPlanets, PLANETS, url } from '../../utils/constants';
 
-class Contact extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            planets: [],
-            lastUpdate: null,
-        };
-    }
+function Contact() {
+    const [planets, setPlanets] = useState([]);
+    const [lastUpdate, setLastUpdate] = useState(null);
 
-    componentDidMount() {
-        this.checkAndUpdatePlanets();
-    }
+    useEffect(() => {
+        checkAndUpdatePlanets();
+    }, []);
 
-    fetchPlanets = () => {
+    const fetchPlanets = () => {
         fetch(`${url}planets`)
             .then((response) => response.json())
-            .then(data => {
-                const planetNames = data.map(planet => planet.name);
+            .then((data) => {
+                const planetNames = data.map((planet) => planet.name);
                 localStorage.setItem(PLANETS, JSON.stringify(planetNames));
                 localStorage.setItem(LAST_UPDATE, Date.now().toString());
-                this.setState({
-                    planets: planetNames,
-                    lastUpdate: Date.now(),
-                });
+                setPlanets(planetNames);
+                setLastUpdate(Date.now());
             })
-            .catch(error => alert(error));
-    }
+            .catch((error) => alert(error));
+    };
 
-    checkAndUpdatePlanets = () => {
-        const lastUpdate = localStorage.getItem(LAST_UPDATE);
-        if (!lastUpdate) {
-            this.fetchPlanets();
+    const checkAndUpdatePlanets = () => {
+        const storedLastUpdate = localStorage.getItem(LAST_UPDATE);
+        if (!storedLastUpdate) {
+            fetchPlanets();
         } else {
-            if (Date.now() - lastUpdate >= periodForPlanets) {
-                this.fetchPlanets();
+            const timeDifference = Date.now() - parseInt(storedLastUpdate);
+            if (timeDifference >= periodForPlanets) {
+                fetchPlanets();
             } else {
                 const storedPlanets = JSON.parse(localStorage.getItem(PLANETS));
-                this.setState({
-                    planets: storedPlanets,
-                    lastUpdate: lastUpdate,
-                });
+                setPlanets(storedPlanets);
+                setLastUpdate(parseInt(storedLastUpdate));
             }
         }
     };
 
-    render() {
-        return (
-            <div>
-                <form className={styles.container}>
-                    <label htmlFor="fname">First Name</label>
-                    <input type="text" id="fname" name="firstname" placeholder="Your name.." className={styles.inputField} />
+    return (
+        <div>
+            <form className={styles.container}>
+                <label htmlFor="fname">First Name</label>
+                <input type="text" id="fname" name="firstname" placeholder="Your name.." className={styles.inputField} />
 
-                    <label htmlFor="lname">Last Name</label>
-                    <input type="text" id="lname" name="lastname" placeholder="Your last name.." className={styles.inputField} />
+                <label htmlFor="lname">Last Name</label>
+                <input type="text" id="lname" name="lastname" placeholder="Your last name.." className={styles.inputField} />
 
-                    <label htmlFor="planet">Planet</label>
-                    <select id="planet" name="planet" className={styles.inputField}>
-                        {this.state.planets.map((planet, index) => (
-                            <option key={index} value={planet}>
-                                {planet}
-                            </option>
-                        ))}
-                    </select>
+                <label htmlFor="planet">Planet</label>
+                <select id="planet" name="planet" className={styles.inputField}>
+                    {planets.map((planet, index) => (
+                        <option key={index} value={planet}>
+                            {planet}
+                        </option>
+                    ))}
+                </select>
 
-                    <label htmlFor="subject">Subject</label>
-                    <textarea id="subject" name="subject" placeholder="Write something.." style={{ height: "200px" }} className={styles.inputField}></textarea>
+                <label htmlFor="subject">Subject</label>
+                <textarea id="subject" name="subject" placeholder="Write something.." style={{ height: '200px' }} className={styles.inputField}></textarea>
 
-                    <input className='btn btn-danger border border-light rounded-pill mx-1 common-button' value="Submit" onClick={()=>alert('Button')}/>
-                </form>
-            </div>
-        );
-    }
+                <input className='btn btn-danger border border-light rounded-pill mx-1 common-button' value="Submit" onClick={() => alert('Button')} />
+            </form>
+        </div>
+    );
 }
 
 export default Contact;
