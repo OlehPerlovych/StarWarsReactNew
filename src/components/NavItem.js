bfa30185576340e211ead78ad1c914b5654d4139
OlehPerlovych <perlovich92@gmail.com>
2023-11-14 18:36:52 +0200

    Add Link in Route to change pages

--- src/components/NavItem.js
+++ src/components/NavItem.js
@@ -1,9 +1,11 @@
+import {Link} from "react-router-dom";
+
 const NavItem = ({text}) => {
     return (
-        <li
-            className="common-button nav-item mx-1 border border-light rounded-pill btn btn-danger"
-            onClick={() => window.location.hash = `#/${text.route}`}> {text.title}
-        </li>
+        <Link className="common-button nav-item mx-1 border border-light rounded-pill btn btn-danger"
+        to={`/${text.route}`}>
+            {text.title}
+        </Link>
     );
 };
 
