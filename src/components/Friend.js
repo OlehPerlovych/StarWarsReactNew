b8442392c29f10158a8b9625cb683517bcd88c95
OlehPerlovych <perlovich92@gmail.com>
2023-11-15 19:04:34 +0200

    Refactor of style. All pictures in section  with border

--- src/components/Friend.js
+++ src/components/Friend.js
@@ -1,16 +1,26 @@
 import React from 'react';
-import {Link} from "react-router-dom";
-import {friends, navItems} from "../utils/constants";
-const Friend = ({item, pos}) =>
-{
+import { Link } from 'react-router-dom';
+import { friends, navItems } from '../utils/constants';
+
+const Friend = ({ item, pos }) => {
     let style = "col-4 p-1";
-    if(pos === 7)
-        style += ' left';
-    else if(pos === 9)
-        style += ' right';
-    return <Link to={`/${navItems[0].route}/${item}`}>
-        <img className={style} src={friends[item].img} alt={friends[item].name}/>
-    </Link>
+
+    if (pos === 7) {
+        style += ' left-bottom-corner';
+    } else if (pos === 9) {
+        style += ' right-bottom-corner';
+    }
+
+    return (
+        <Link to={`/${navItems[0].route}/${item}`}>
+            <img
+                className={`img-fluid w-100 h-100 ${style}`}
+                src={friends[item].img}
+                alt={friends[item].name}
+                style={pos === 7 ? { borderBottomLeftRadius: '10%' } : pos === 9 ? { borderBottomRightRadius: '10%' } : {}}
+            />
+        </Link>
+    );
 };
 
 export default Friend;
--- src/components/Friends.js
+++ src/components/Friends.js
@@ -3,13 +3,21 @@ import {characters} from "../utils/constants";
 import Friend from "./Friend";
 import {SWContext} from "../utils/context";
 
-const Friends = () =>
-{
-    const {heroFromPath} = useContext(SWContext);
+const Friends = () => {
+    const { heroFromPath } = useContext(SWContext);
+
     return (
         <section className="float-end w-50 border border-light rounded-bottom-3 row m-1">
             <h3 className="text-center col-12 text-uppercase">Friends</h3>
-            {characters.filter(item => item !== heroFromPath).map((item, index) => <Friend key={index} item={item} pos={index+1}/>)}
+            <div className="row">
+                {characters
+                    .filter(item => item !== heroFromPath)
+                    .map((item, index) => (
+                        <div key={index} className="col-4 mb-3"> {/* Измените размеры колонок в соответствии с вашими требованиями */}
+                            <Friend key={index} item={item} pos={index + 1} />
+                        </div>
+                    ))}
+            </div>
         </section>
     );
 };
